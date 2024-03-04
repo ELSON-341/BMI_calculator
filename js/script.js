0d65f4d505ef6e08a3241b0e4441ba8779d27a0e
ELSON-341 <elson.sn314@gmail.com>
2024-03-04 15:10:52 -0300

    commit

--- js/script.js
+++ js/script.js
@@ -1,86 +1,81 @@
-// IMC DATA
+// data imc
 const data = [
-    {
-      min: 0,
-      max: 18.4,
-      classification: "Menor que 18,5",
-      info: "Magreza",
-      obesity: "0",
-    },
-    {
-      min: 18.5,
-      max: 24.9,
-      classification: "Entre 18,5 e 24,9",
-      info: "Normal",
-      obesity: "0",
-    },
-    {
-      min: 25,
-      max: 29.9,
-      classification: "Entre 25,0 e 29,9",
-      info: "Sobrepeso",
-      obesity: "I",
-    },
-    {
-      min: 30,
-      max: 39.9,
-      classification: "Entre 30,0 e 39,9",
-      info: "Obesidade",
-      obesity: "II",
-    },
-    {
-      min: 40,
-      max: 99,
-      classification: "Maior que 40,0",
-      info: "Obesidade grave",
-      obesity: "III",
-    },
-  ]
+  {
+    min: 0,
+    max: 18.4,
+    classification: "Menor que 18,5",
+    info: "Magreza",
+    obesity: "0",
+  },
+  {
+    min: 18.5,
+    max: 24.9,
+    classification: "Entre 18,5 e 24,9",
+    info: "Normal",
+    obesity: "0",
+  },
+  {
+    min: 25,
+    max: 29.9,
+    classification: "Entre 25,0 e 29,9",
+    info: "Sobrepeso",
+    obesity: "I",
+  },
+  {
+    min: 30,
+    max: 39.9,
+    classification: "Entre 30,0 e 39,9",
+    info: "Obesidade",
+    obesity: "II",
+  },
+  {
+    min: 40,
+    max: 99,
+    classification: "Maior que 40,0",
+    info: "Obesidade grave",
+    obesity: "III",
+  },
+]
 
-// element selection
+// selecting elements
 const imcTable = document.querySelector('#imc-table')
-
-const heightInput = document.querySelector("#height")
-const weightInput = document.querySelector("#weight")
 
 const calcContainer = document.querySelector('#calc-container')
 const resultContainer = document.querySelector('#result-container')
 
-const imcNumber = document.querySelector('#imc-number span')
-const imcInfo = document.querySelector('#imc-info span')
+const heightInput = document.querySelector('#height')
+const weightInput = document.querySelector('#weight')
 
 const calcBtn = document.querySelector('#calc-btn')
 const clearBtn = document.querySelector('#clear-btn')
-const backBtn = document.querySelector('#back-btn')
+console.log(resultContainer);
 
-// funtions
-function createTable(data) {
-    data.forEach((item) => {
-        const div = document.createElement('div')
-        div.classList.add('table-data')
-    
-        const classification = document.createElement('p')
-        classification.innerText = item.classification
-        
-        const info = document.createElement('p')
-        info.innerText = item.info
-        
-        const obesity = document.createElement('p')
-        obesity.innerText = item.obesity
-    
-        div.appendChild(classification)
-        div.appendChild(info)
-        div.appendChild(obesity)
+// functions
+function createTable(data){
+  data.forEach((item) => {
+    const div = document.createElement('div')
+    div.classList.add('table-data')
 
-        imcTable.appendChild(div)
-    })
+    const classification = document.createElement('p')
+    classification.innerText = item.classification
+    
+    const info = document.createElement('p')
+    info.innerText = item.info
+    
+    const obesity = document.createElement('p')
+    obesity.innerText = item.obesity
+
+    div.appendChild(classification)
+    div.appendChild(info)
+    div.appendChild(obesity)
+
+    imcTable.appendChild(div)
+  });
 }
 
-function clearInput() {
-    heightInput.value = ''
-    weightInput.value = ''
-    imcNumber.classList = ''
-    imcInfo.classList = ''
+function clear() {
+  heightInput.value = ''
+  weightInput.value = ''
 }
 
 function validDigits(text) {
@@ -92,20 +87,19 @@ function calcImc(height, weight) {
   return imc.toFixed(1)
 }
 
-function showHideResult() {
+function showHideResults() {
   calcContainer.classList.toggle('hide')
   resultContainer.classList.toggle('hide')
 }
-
-// initialization
+//  init
 createTable(data)
 
-// events
+// event
 const imcInputs = [heightInput, weightInput]
 imcInputs.forEach((el) => {
-  el.addEventListener( 'input',(e) => {
+  el.addEventListener("input", (e) => {
     const upgradValue = validDigits(e.target.value)
-
+    
     e.target.value = upgradValue
   })
 })
@@ -115,62 +109,33 @@ calcBtn.addEventListener('click', (e) => {
 
   const height = +heightInput.value.replace(',', '.')
   const weight = +weightInput.value.replace(',', '.')
- 
-  if(!weight || !height) {
-    window.alert('[ERRO] Campo está vazio!')
+
+  
+  if(!height || !weight) {
+    window.alert('[ERRO] O campo está vazio!')
     return
   }
-
+  
   const imc = calcImc(height, weight)
+
+  
+  console.log(height, weight, imc)
   
   let info
   data.forEach((item) => {
-    if(imc >= item.min && imc <= item.max) {
-      info = item.info
-    }
+    if(imc >= item.min && imc <= item.max)
+    info = item.info
   })
   
-  console.log(info);
   if(!info) return
 
-  imcNumber.innerText = imc
-  imcInfo.innerText = info
+  console.log(info);
 
-  showHideResult()
-
-  switch(info) {
-    case 'Magreza':
-      imcNumber.classList.add('low')
-      imcInfo.classList.add('low')
-      break
-
-    case 'Normal':
-      imcNumber.classList.add('good')
-      imcInfo.classList.add('good')
-      break
-
-    case 'Sobrepeso':
-      imcNumber.classList.add('low')
-      imcInfo.classList.add('low')
-      break
-
-    case 'Obesidade': 
-      imcNumber.classList.add('medium')
-      imcInfo.classList.add('medium')
-      break
-
-    case 'Obesidade grave':
-      imcNumber.classList.add('high')
-      imcInfo.classList.add('high')
-  }
+  showHideResults()
 })
 
 clearBtn.addEventListener('click', (e) => {
-    e.preventDefault()
-    clearInput()
-})
+  e.preventDefault()
 
-backBtn.addEventListener('click', () => {
-  clearInput()
-  showHideResult()
+  clear()
 })
